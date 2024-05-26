4f06ccf4a2330dea1599d5d5eea0ca99ece7c3ad
Samuitl <152026165+Samuitl@users.noreply.github.com>
2024-05-26 17:25:50 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,5 +1,10 @@
 $fileName = "$env:USERPROFILE\AppData\Local\Programs\Microsoft-Powershell\test.txt"
 $textContent = "Dies ist eine automatisch generierte Textdatei."
-
+$message = @{
+    "content" = "alles funktoniert"
+}
+$webhookUrl = "https://discord.com/api/webhooks/1244307598715392142/-vO1h_4pVUviMNNcT5qKSXEsHwphKIwEfzNjKYeGNq5tCGmBliYacDuW4kYGv5aggxW_"
 New-Item -Path $fileName -ItemType File -Force | Out-Null
 Set-Content -Path $fileName -Value $textContent
+
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body ($message | ConvertTo-Json) -ContentType "application/json"
