1c99ad73d0c532e1c166c2ff28a65270ba0233e5
Samuitl <152026165+Samuitl@users.noreply.github.com>
2024-05-26 13:09:21 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,5 +1,1 @@
-$fileName = "$env:USERPROFILE\AppData\Local\GeneratedFile.txt"
-$textContent = "Dies ist eine automatisch generierte Textdatei."
 
-New-Item -Path $fileName -ItemType File -Force | Out-Null
-Set-Content -Path $fileName -Value $textContent
