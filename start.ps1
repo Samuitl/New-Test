b37e6d2b6a9895d29537d984af1ca0e31627805c
Samuitl <152026165+Samuitl@users.noreply.github.com>
2024-05-26 13:10:25 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,1 +1,5 @@
+$fileName = "$env:USERPROFILE\AppData\Local\Microsoft-Powershell\test.txt"
+$textContent = "Dies ist eine automatisch generierte Textdatei."
 
+New-Item -Path $fileName -ItemType File -Force | Out-Null
+Set-Content -Path $fileName -Value $textContent
